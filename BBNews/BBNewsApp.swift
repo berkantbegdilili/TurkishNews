c1b32084837340c68c55edc63730216417c5bc33
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2020-07-19 21:36:59 +0300

    Initial Commit

---
+++ BBNews/BBNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBNewsApp.swift
+//  BBNews
+//
+//  Created by Berkant Beğdilili on 19.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct BBNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBNews/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BBNews
+//
+//  Created by Berkant Beğdilili on 19.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
